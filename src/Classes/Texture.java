fc7dd0782c7204b8ace3d3f81318516e3731d436
TZR-Redstonemaniac21 <khaliddeen2007@gmail.com>
2023-09-05 13:11:37 +0300

    Fixed Sonar Binaries

--- src/Classes/Texture.java
+++ src/Classes/Texture.java
@@ -27,24 +27,25 @@ public class Texture {
     public static Image BLACK_KING = null; //NOSONAR
     public static Image BLACK_QUEEN = null; //NOSONAR
 
-    private static final String FOLDER = "/home/khalid/IntellijProjects/Chess/src/Textures/White_Pieces/";
+    private static final String WHITE_FOLDER = "/home/khalid/IntellijProjects/Chess/src/Textures/White_Pieces/";
+    private static final String BLACK_FOLDER = "/home/khalid/IntellijProjects/Chess/src/Textures/Black_Pieces/";
     private static final Logger LOGGER = Logger.getLogger(Texture.class.getName());
 
     public static void Init(){
         try {
-            WHITE_PAWN = ImageIO.read(new File(FOLDER, "White_Pawn.png"));
-            WHITE_KNIGHT = ImageIO.read(new File(FOLDER, "White_Knight.png"));
-            WHITE_BISHOP = ImageIO.read(new File(FOLDER, "White_Bishop.png"));
-            WHITE_ROOK = ImageIO.read(new File(FOLDER, "White_Rook.png"));
-            WHITE_KING = ImageIO.read(new File(FOLDER, "White_King.png"));
-            WHITE_QUEEN = ImageIO.read(new File(FOLDER, "White_Queen.png"));
+            WHITE_PAWN = ImageIO.read(new File(WHITE_FOLDER, "White_Pawn.png"));
+            WHITE_KNIGHT = ImageIO.read(new File(WHITE_FOLDER, "White_Knight.png"));
+            WHITE_BISHOP = ImageIO.read(new File(WHITE_FOLDER, "White_Bishop.png"));
+            WHITE_ROOK = ImageIO.read(new File(WHITE_FOLDER, "White_Rook.png"));
+            WHITE_KING = ImageIO.read(new File(WHITE_FOLDER, "White_King.png"));
+            WHITE_QUEEN = ImageIO.read(new File(WHITE_FOLDER, "White_Queen.png"));
 
-            BLACK_PAWN = ImageIO.read(new File(FOLDER, "Black_Pawn.png"));
-            BLACK_KNIGHT = ImageIO.read(new File(FOLDER, "Black_Knight.png"));
-            BLACK_BISHOP = ImageIO.read(new File(FOLDER, "Black_Bishop.png"));
-            BLACK_ROOK = ImageIO.read(new File(FOLDER, "Black_Rook.png"));
-            BLACK_KING = ImageIO.read(new File(FOLDER, "Black_King.png"));
-            BLACK_QUEEN = ImageIO.read(new File(FOLDER, "Black_Queen.png"));
+            BLACK_PAWN = ImageIO.read(new File(BLACK_FOLDER, "Black_Pawn.png"));
+            BLACK_KNIGHT = ImageIO.read(new File(BLACK_FOLDER, "Black_Knight.png"));
+            BLACK_BISHOP = ImageIO.read(new File(BLACK_FOLDER, "Black_Bishop.png"));
+            BLACK_ROOK = ImageIO.read(new File(BLACK_FOLDER, "Black_Rook.png"));
+            BLACK_KING = ImageIO.read(new File(BLACK_FOLDER, "Black_King.png"));
+            BLACK_QUEEN = ImageIO.read(new File(BLACK_FOLDER, "Black_Queen.png"));
         } catch (IOException e) {
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
         }
